953b6f047435f98dacbabd10a0c85c8a3deb60a5
Kal-el-s <najut499103192@outlook.com>
2024-08-15 16:24:19 +0800

    first commit

---
+++ ncnn-android-yolov8/app/build/intermediates/cxx/Debug/2o2j525z/logs/arm64-v8a/build_command_yolov8ncnn.bat
@@ -0,0 +1,5 @@
+@echo off
+"D:\\Android_SDK\\cmake\\3.10.2.4988404\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Administrator\\Desktop\\code\\ncnn-android-yolov8\\app\\.cxx\\Debug\\2o2j525z\\arm64-v8a" ^
+  yolov8ncnn
---
+++ ncnn-android-yolov8/app/build/intermediates/cxx/RelWithDebInfo/184r2p2p/logs/arm64-v8a/build_command_yolov8ncnn.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Administrator\\AppData\\Local\\Android\\Sdk\\cmake\\3.10.2.4988404\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Administrator\\Desktop\\code\\ncnn-android-yolov8\\app\\.cxx\\RelWithDebInfo\\184r2p2p\\arm64-v8a" ^
+  yolov8ncnn
